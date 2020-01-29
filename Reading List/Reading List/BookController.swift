9b16d08215efd7f6a252f444e8ed2c2bece71930
StephanieBallard <58450565+StephanieBallard@users.noreply.github.com>
2020-01-28 18:52:06 -0800

    Add files via upload

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,97 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Stephanie Ballard on 1/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController: Codable {
+    
+//    init() {
+//       loadFromPersistentStore()
+//    }
+    
+    
+    var books: [Book]
+    
+    
+    
+    
+    func createBook(with title: String, reason: String) {
+        let book = Book(title: title, reasonToRead: reason)
+        
+        books.append(book)
+        
+        saveToPersistanceStore()
+        
+    }
+    
+    
+    
+    
+    
+    var readingListURL: URL? {
+        
+        let fileManager = FileManager.default
+            
+        guard let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil}
+        let booksURL = documentsDir.appendingPathComponent("ReadingList.plist")
+        
+        return booksURL
+        
+    }
+
+    func saveToPersistanceStore() {
+        
+        guard let fileURL = readingListURL else { return }
+        
+        do {
+            
+            let encoder = PropertyListEncoder()
+            
+            let booksData = try encoder.encode(books)
+            
+            try booksData.write(to: fileURL)
+            
+        } catch {
+            print("Error encoding books array: \(books)")
+            
+        }
+        
+    }
+    
+    func loadFromPersistentStore() {
+        guard let fileURL = readingListURL else { return }
+        
+        do {
+            
+            let booksData = try Data(contentsOf: fileURL)
+            
+            let decoder = PropertyListDecoder()
+            
+            let booksArray = try decoder.decode([Book].self, from: booksData)
+            
+            self.books = booksArray
+            
+        } catch {
+            print("Error decoding books: \(error)")
+            
+        }
+        
+    }
+    
+    func removeBook(named book: Book) {
+      guard let index = books.firstIndex(of: book) else { return }
+      books.remove(at: index)
+      
+        saveToPersistanceStore()
+        
+       
+    }
+
+}
+
+
